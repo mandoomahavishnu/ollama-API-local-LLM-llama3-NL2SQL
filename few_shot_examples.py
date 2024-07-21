bf07f862831f6b81d084d852034f57c047d3741d
mandoomahavishnu <166954779+mandoomahavishnu@users.noreply.github.com>
2024-07-21 12:37:59 -0700

    Add files via upload

---
+++ few_shot_examples.py
@@ -0,0 +1,7 @@
+few_shot_examples = [
+    {"natural_language": "List all users.", "sql_query": "SELECT username,fname,lname FROM user;"},
+    {"natural_language": "How many items are there?",
+     "sql_query": "SELECT COUNT(*) FROM products;"},
+    
+]
+
